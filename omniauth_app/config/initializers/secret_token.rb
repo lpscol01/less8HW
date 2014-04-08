849dedf60ee66ed1ff8cb542f524910eb6ac09fd
Lisa Pscolkoski <lpscol01@gmail.com>
2014-04-08 16:42:31 -0400

    lesson 8 hw - original submission

---
+++ omniauth_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OmniauthApp::Application.config.secret_key_base = 'e4050a17021a879c56567659d57d0733c985e4bc48bbba79a1cf7a1e01ef8cb222781f8ef1bf73afbe68216f98b22d2c4561d99c8a6860d4bc34764ecfd872b7'
